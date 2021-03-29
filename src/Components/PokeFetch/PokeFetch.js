f4f16cb3d2ef51127c7df30c3f3a91a7575d0d73
Daniel Buergler <daniel.jbuergler@gmail.com>
2021-03-29 13:01:03 -0400

    finished challenge

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -3,14 +3,18 @@ import './PokeFetch.css';
 
 
 class PokeFetch extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      currentCount: 10,
+      intervalId: '',
+      pokeStyle: false
     }
   }
+
 
   fetchPokemon() {
     let min = Math.ceil(1);
@@ -24,23 +28,50 @@ class PokeFetch extends Component {
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
+          currentCount: 10,
+          pokeStyle: false
         })
       })
       .catch((err) => console.log(err))
   }
 
+  
+  
+  timer() {
+    let intervalId = 
+    setInterval (() => this.tick(), 1000); 
+    this.setState({intervalId: intervalId})
+  }
+  
+  tick(){
+    if (this.state.currentCount > 0){
+      this.setState({ currentCount: this.state.currentCount -1 })
+    } else{
+      clearInterval(this.intervalId)
+      this.setState({pokeStyle: true})
+    }
+  }
+  
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.pokeName !== this.state.pokeName)
+    this.timer();
+  }
+
+
   render() {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <h1 className={'timer'} >Timer Display:{this.state.currentCount}</h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          <img className={'pokeImg'} style={this.state.pokeStyle ? {filter: "brightness(100%)"}:{filter: "brightness(0%)"}} src={this.state.pokeSprite} alt="" />
+          <h1 className={'pokeName'} style={this.state.pokeStyle ? {display: ''}: {display: "none"}}>{this.state.pokeName} </h1>
         </div>
       </div>
     )
   }
 }
+
+
 
 export default PokeFetch;
